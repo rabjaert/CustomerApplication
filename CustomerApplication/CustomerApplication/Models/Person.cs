282ca96b02d9d80edba266cd92f7ec3423ec075e
rabjaert <55789494+rabjaert@users.noreply.github.com>
2020-04-07 13:20:47 +0200

    Satt opp prosjekt

---
+++ CustomerApplication/CustomerApplication/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApplication.Models
+{
+    public class Person
+    {
+
+        protected int PersonID { get; set; }
+        protected string FirstName { get; set; }
+        protected string LastName { get; set; }
+        protected int Age { get; set; }
+
+        [EmailAddress]
+        protected string Email { get; set;}
+
+        protected int PhoneNumber { get; set; }
+
+        protected Role RoleCompany { get; set; }
+
+        protected Company Company { get; set; }
+
+    }
+}
